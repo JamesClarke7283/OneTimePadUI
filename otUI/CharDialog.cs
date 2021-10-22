2702ea7197f65e16dc99c14bc95215e3842e20ed
Ryan Jones <rj214@canterbury.ac.uk>
2021-10-22 11:46:51 +0000

    Created the CharDialog File

---
+++ otUI/CharDialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+using otlib;
+public class CharDialog : Gtk.Dialog
+{
+    [UI] Gtk.TextView KeyOutputView = new Gtk.TextView();
+    [UI] Gtk.SpinButton keyLength;
+
+    Builder builder;
+
+    public static CharDialog Create()
+    {
+        Builder builder = new Builder(null, "otUI.interfaces.GenerateKeys.glade", null);
+        return new CharDialog(builder, builder.GetObject("generatekeysdialog").Handle);
+    }
+
+    protected CharDialog(Builder builder, IntPtr handle) : base(handle)
+    {
+        this.builder = builder;
+
+        builder.Autoconnect(this);
+        AddButton("Close", ResponseType.Close);
+    }
+
+    protected void on_Save_clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void on_UpperLowerNum_clicked(object sender, EventArgs e)
+    {
+
+    }
+    protected void on_UpperNum_clicked(object sender, EventArgs e)
+    {
+
+    }
+    protected void on_Numerical_clicked(object sender, EventArgs e)
+    {
+
+    }
+}
