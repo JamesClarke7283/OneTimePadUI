49d0fcab1daef97624896e8f98721bd72ebe4dc4
Ryan Jones <rj214@canterbury.ac.uk>
2021-10-22 19:01:22 +0000

    Update CharDialog.cs
    Just need to link to settings

--- otUI/CharDialog.cs
+++ otUI/CharDialog.cs
@@ -1,16 +1,22 @@
-﻿using System;
+using System;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 using otlib;
 public class CharDialog : Gtk.Dialog
 {
+    [UI] Gtk.RadioButton UpperLowerNum;
+    [UI] Gtk.RadioButton UpperNum;
+    [UI] Gtk.RadioButton Numerical;
+    [UI] Gtk.Entry Custom = new Gtk.Entry();
 
     Builder builder;
 
+    public string charset;
+
     public static CharDialog Create()
     {
-        Builder builder = new Builder(null, "otUI.interfaces.CharSet.glade", null);
-        return new CharDialog(builder, builder.GetObject("chardialog").Handle);
+        Builder builder = new Builder(null, "otUI.interfaces.GenerateKeys.glade", null);
+        return new CharDialog(builder, builder.GetObject("generatekeysdialog").Handle);
     }
 
     protected CharDialog(Builder builder, IntPtr handle) : base(handle)
@@ -18,24 +24,38 @@ public class CharDialog : Gtk.Dialog
         this.builder = builder;
 
         builder.Autoconnect(this);
-        //AddButton("Close", ResponseType.Close);
+        AddButton("Close", ResponseType.Close);
     }
 
-    protected void on_Save_clicked(object sender, EventArgs e)
+    protected void On_Custom_changed(object sender, EventArgs e)
     {
+        string charset = Custom.Text;
+        this.charset = charset;
+    }
+
+    protected void On_UpperLowerNum_clicked(object sender, EventArgs e)
+    {
+        string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        this.charset = charset;
 
     }
 
-    protected void on_UpperLowerNum_clicked(object sender, EventArgs e)
+    protected void On_UpperNum_clicked(object sender, EventArgs e)
     {
+        string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        this.charset = charset;
 
     }
-    protected void on_UpperNum_clicked(object sender, EventArgs e)
+
+    protected void On_Numerical_clicked(object sender, EventArgs e)
     {
+        string charset = "0123456789";
+        this.charset = charset;
 
     }
-    protected void on_Numerical_clicked(object sender, EventArgs e)
-    {
 
+    protected void On_Save_clicked(object sender, EventArgs e)
+    {
+        //still working on this
     }
 }
