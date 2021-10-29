1b084b5e34b9a5504e484d6305b2ab1e59cf1b6e
J C <jc1222@canterbury.ac.uk>
2021-10-29 11:16:20 +0100

    Finished CharsetDialog, closes #11

--- otUI/CharDialog.cs
+++ otUI/CharDialog.cs
@@ -15,8 +15,8 @@ public class CharDialog : Gtk.Dialog
 
     public static CharDialog Create()
     {
-        Builder builder = new Builder(null, "otUI.interfaces.GenerateKeys.glade", null);
-        return new CharDialog(builder, builder.GetObject("generatekeysdialog").Handle);
+        Builder builder = new Builder(null, "otUI.interfaces.CharsetDialog.glade", null);
+        return new CharDialog(builder, builder.GetObject("charsetdialog").Handle);
     }
 
     protected CharDialog(Builder builder, IntPtr handle) : base(handle)
@@ -29,33 +29,26 @@ public class CharDialog : Gtk.Dialog
 
     protected void On_Custom_changed(object sender, EventArgs e)
     {
-        string charset = Custom.Text;
-        this.charset = charset;
+        charset = Custom.Text;
     }
 
     protected void On_UpperLowerNum_clicked(object sender, EventArgs e)
     {
-        string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        this.charset = charset;
-
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     }
 
     protected void On_UpperNum_clicked(object sender, EventArgs e)
     {
-        string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        this.charset = charset;
-
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     }
 
     protected void On_Numerical_clicked(object sender, EventArgs e)
     {
-        string charset = "0123456789";
-        this.charset = charset;
-
+        charset = "0123456789";
     }
 
     protected void On_Save_clicked(object sender, EventArgs e)
     {
-        //still working on this
+        otlib.Settings.charset = charset;
     }
 }
