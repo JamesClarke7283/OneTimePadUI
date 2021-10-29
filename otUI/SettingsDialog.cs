d75e60fbf30e37288a62d08f9fb77cbf77d86630
J C <jc1222@canterbury.ac.uk>
2021-10-29 14:01:42 +0100

    Made charset setting in setting instead of button

--- otUI/SettingsDialog.cs
+++ otUI/SettingsDialog.cs
@@ -9,6 +9,13 @@ public class SettingsDialog : Gtk.Dialog
 
     Builder builder;
 
+    public string charset;
+
+    [UI] Gtk.RadioButton UpperLowerNum;
+    [UI] Gtk.RadioButton UpperNum;
+    [UI] Gtk.RadioButton Numerical;
+    [UI] Gtk.Entry Custom = new Gtk.Entry();
+
     public static SettingsDialog Create()
     {
         Builder builder = new Builder(null, "otUI.interfaces.SettingsDialog.glade", null);
@@ -23,11 +30,29 @@ public class SettingsDialog : Gtk.Dialog
         AddButton("Close", ResponseType.Close);
     }
 
-    protected void On_CharsetButton_clicked(object sender, EventArgs e)
+    protected void On_Save_clicked(object sender, EventArgs e)
     {
-        CharDialog cd = CharDialog.Create();
-        cd.Run();
-        cd.Destroy();
+        otlib.Settings.charset = charset;
+    }
+
+    protected void On_Custom_changed(object sender, EventArgs e)
+    {
+        charset = Custom.Text;
+    }
+
+    protected void On_UpperLowerNum_clicked(object sender, EventArgs e)
+    {
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    }
+
+    protected void On_UpperNum_clicked(object sender, EventArgs e)
+    {
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    }
+
+    protected void On_Numerical_clicked(object sender, EventArgs e)
+    {
+        charset = "0123456789";
     }
 
 
