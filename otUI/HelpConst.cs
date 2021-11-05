4919a3c8b01a39ccc887a6a876d0058b26aaaaf7
shvbby <s.anthony@live.co.uk>
2021-11-05 10:59:00 +0000

    Edited the Help Dialog

--- otUI/HelpConst.cs
+++ otUI/HelpConst.cs
@@ -12,8 +12,6 @@ namespace otUI
                                                 "Welcome to the Generate Keys feature of OneTimePad.\n" +
                                                 "\nNumber Selection.\n" +
                                                 "Selects the amount of characters in the produced key. Use the + and - sign to add or subtract the amount of characters, or enter manually.\n" +
-                                                "\nCharSet Button.\n" +
-                                                "Allows users to pick what Character Set to use. Choose between Uppercase, lowercase, numerical or custom.\n" +
                                                 "\nGenerate Button\n " +
                                                 "Generates the key and displays it in the box below.\n" +
                                                 "\nClose Button\n" +
@@ -29,11 +27,16 @@ namespace otUI
                                                 "Will Encrypt the text inputted into the Message/ Ciphertext Input Box.\n" +
                                                 "\nDecrypt Button\n" +
                                                 "Will Decrypt the text inputted into the Message/ Ciphertext Input Box.\n" +
-                                                "\nCharSet Button\n" +
-                                                "Allows users to pick what Character Set to use. Choices available are uppercase, lowercase, numerical or custom.\n" +
                                                 "\nOutput Box\n" +
                                                 "Will Display the Finished text, whether Encypted or Decrypted.\n";
 
-        public static string SettingsHelp = "Test\n";
+        public static string SettingsHelp = "Settings Help\n"+
+                                            "Welcome to the Preferences help box!\n"+
+                                            "\nChange Charset\n"+
+                                            "\nCode Charset\n"+
+                                            "Input the Character Set that you would like to be used in the Key, in the 'Generate Keys' Feature. If Custom is selected, then input the characters you would like to be used in the 'Custom' Box.\n"+
+                                            "\nText Charset\n"+
+                                            "Input the Character Set needed to -\n"+
+                                            "\nRNG Device\n";
     }
 }
