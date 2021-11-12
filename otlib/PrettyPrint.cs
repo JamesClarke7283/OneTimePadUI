367759c4c54d14948b951c55f33e3d0d4c89aa50
J C <jc1222@canterbury.ac.uk>
2021-11-12 15:15:15 +0000

    Added Grid print pretty print

--- otlib/PrettyPrint.cs
+++ otlib/PrettyPrint.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace otlib
 {
     public class PrettyPrint
@@ -17,6 +19,15 @@ namespace otlib
                 key = key.Insert(i, separator.ToString());
                 i++;
             }
+            return key;
+        }
+
+        public string GridPrettify(string key) 
+        {
+                key = string.Join(Environment.NewLine, key.Split()
+                .Select((word, index) => new { word, index })
+                .GroupBy(x => x.index / 5)
+                .Select(grp => string.Join(" ", grp.Select(x => x.word))));
             return key;
         }
 
