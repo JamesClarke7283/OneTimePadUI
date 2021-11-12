a9541059a38653fd961423c0e9ef73ab3aee2394
Leo Spratt <enchant97@users.noreply.github.com>
2021-11-12 15:22:07 +0000

    add pretty print to test code

--- otUI/PrintPreview.cs
+++ otUI/PrintPreview.cs
@@ -2,6 +2,7 @@
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 using Cairo;
+using otlib;
 
 namespace otUI
 {
@@ -44,6 +45,8 @@ namespace otUI
                     };
 
                     string text = "8873 2357 3753 1118 2547 3653 2752 4692 5408 1538 3552 1509 2354 2299 2432 7893 8170 9799 5717 3368 5077 5537 5976 8135 5591 2546 4122 5812 7273 6762 6071 3255 5459 8214 9358 2200 7359 4564 1437 6425 9498 6275 4479 4384 1644 8145 3128 3081 3011 0339";
+                    PrettyPrint pp = new();
+                    text = pp.GridPrettify(text);
 
                     Pango.Layout layout = CreatePangoLayout(text);
                     layout.FontDescription = font;
