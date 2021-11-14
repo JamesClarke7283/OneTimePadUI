1420f1478950ff253d2ae9e5ee2fd2b5c02df44d
J C <jc1222@canterbury.ac.uk>
2021-11-14 13:14:00 +0000

    Added starter code to make resource icons display outside working directory

--- otUI/MainMenu.cs
+++ otUI/MainMenu.cs
@@ -2,22 +2,41 @@
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 using otlib;
+using Gdk;
 
 public partial class MainMenu : Gtk.Window
 {
     Builder builder;
     private AppSettings appSettings;
 
+
+    public static Gdk.Pixbuf SharedAppIcon = new Gdk.Pixbuf(null,"otUI.assets.lock.ico");
+    public static Gdk.Pixbuf GenKeyIcon = new Gdk.Pixbuf(null, "otUI.assets.key.ico");
+
+
+
+    public static Gtk.Image GenKeyImg = new Gtk.Image(pixbuf: GenKeyIcon);
+
+    [UI] Gtk.Button keygen = new Gtk.Button();
+    [UI] Gtk.Button cryptodecrypt = new Gtk.Button();
+    [UI] Gtk.Button SettingsButton = new Gtk.Button();
+
+
+
     public static MainMenu Create(AppSettings appSettings)
     {
         Builder builder = new Builder(null, "otUI.interfaces.MainWindow.glade", null);
         return new MainMenu(builder, builder.GetObject("mainmenu").Handle, appSettings);
+        
     }
 
     protected MainMenu(Builder builder, IntPtr handle, AppSettings appSettings) : base(handle)
     {
         this.builder = builder;
         this.appSettings = appSettings;
+        Icon = MainMenu.SharedAppIcon;
+
+        keygen.Image = GenKeyImg;
 
         builder.Autoconnect(this);
         DeleteEvent += OnDeleteEvent;
