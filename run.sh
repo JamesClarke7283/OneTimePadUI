bd1bbcc0aee5dfbad7c4d0ec1426d97b483351c7
James Clarke <james@james-clarke.ynh.fr>
2022-10-10 16:02:39 +0100

    Add run script to program

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Program which creates a virtual environment with dependencies (if not exists). And runs the OneTimePadUI program 
+if [ -d .venv ]
+then
+    echo "Found Virtual Environment"
+    source .venv/bin/activate
+else
+    echo "No Virtual Environment found"
+    python3.10 -m venv .venv
+    source .venv/bin/activate
+    echo "Installing dependencies..."
+    pip3 install -r requirements.txt
+fi
+echo "Running OneTimePadUI..."
+python3.10 main.py
